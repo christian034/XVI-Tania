68f439c0dae5a581baf3d7caabad2fdb90c7de5a
Christian Alvarez <christianaah034@gmail.com>
2024-12-02 20:38:31 -0600

    se añadio netlify@astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify(),
 });
